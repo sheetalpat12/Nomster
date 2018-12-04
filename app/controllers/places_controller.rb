2fe2fd159e838778baacf2114d9de9f0de41b1f8
sheetal.sompura1985@gmail.com <sheetal.sompura1985@gmail.com>
2018-12-04 19:49:35 +0000

    lesson 12 completed adding simple form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,12 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
-
+     @places = Place.order("id").page(params[:page]).per_page(5)
+     # @places = Place.search(params[:name], params[:page])
   end
+
+  def new
+    @place = Place.new
+  end
+
 end
