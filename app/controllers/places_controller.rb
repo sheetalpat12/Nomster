14d75a32a823b265c1e39f383c87b095b19c35be
sheetal.sompura1985@gmail.com <sheetal.sompura1985@gmail.com>
2018-12-06 23:28:20 +0000

    added db index userId to places table

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+before_action :authenticate_user!, only: [:new, :create]
 
   def index
      @places = Place.order("id").page(params[:page]).per_page(5)
