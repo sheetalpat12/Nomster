84b654d3c5533c15fd9edb991e5b477e82b877c1
sheetal.sompura1985@gmail.com <sheetal.sompura1985@gmail.com>
2018-12-13 07:54:36 +0000

    lesson 32-adding comments form on the show page

--- db/migrate/20181213011105_create_comments.rb
+++ db/migrate/20181213011105_create_comments.rb
@@ -1,4 +1,4 @@
-class CreateComments < ActiveRecord::Migration[5.2]
+class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
       t.text :message
@@ -10,6 +10,5 @@ class CreateComments < ActiveRecord::Migration[5.2]
 
     add_index :comments, [:user_id, :place_id]
     add_index :comments, :place_id
-    end
   end
 end
