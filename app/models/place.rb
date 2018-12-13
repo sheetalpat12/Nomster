37c2deff6daebbe135f16a94f4e7431edf7c019d
sheetal.sompura1985@gmail.com <sheetal.sompura1985@gmail.com>
2018-12-13 01:20:17 +0000

    lesson 31-adding comments to the form

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  
   geocoded_by :address
   after_validation :geocode
+  
   validates :name, presence: true, length: { minimum: 4 }
   validates :address, presence: true, uniqueness: true
   validates :description, presence: true, length: { maximum: 500 }
